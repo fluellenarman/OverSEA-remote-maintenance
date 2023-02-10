a0108c3c44af33e568edf3dddbd12ce9e09e1a94
iikare <13582030+iikare@users.noreply.github.com>
2023-02-09 18:12:01 -0800

    automatically initialize server resources

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "MAKE SURE TO \"npm install\" BEFORE YOU START THE SERVER"
-
-
 export DEBUG=dss*
 set DEBUG=dss*
+
+if [[ ! -e node-dss/node_modules ]]
+then
+	echo "you have not installed the required node packages"
+	echo "MAKE SURE TO \'npm install\' in \'node-css\' directory BEFORE YOU START THE SERVER"
+	echo "installing packages for you"
+	cd node-dss && npm install
+fi
 
 cd node-dss && npm start
